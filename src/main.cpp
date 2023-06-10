06d46d0ff3fccfd5a2961a6b694d7fd5f3b8efe9
Jj Kwan <49112476+Tringpore@users.noreply.github.com>
2023-06-10 16:17:24 +0800

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include "main.h"
+#include "pros/adi.h"
+#include "pros/adi.hpp"
+#include "pros/misc.h"
+#include "pros/motors.h"
+#include "globals.hpp"
+#include "mech_lib.hpp"
+
+void on_center_button() {
+
+}
+
+void initialize() {
+	
+}
+
+
+void disabled() {
+
+}
+
+
+void competition_initialize() {
+
+}
+
+
+void autonomous() {
+
+}
+
+
+void opcontrol() {
+
+}
