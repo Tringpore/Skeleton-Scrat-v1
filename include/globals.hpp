1a3b4fff5afc42a73e477f133b11442dd9fd6561
Jj Kwan <tringpore@gmail.com>
2023-06-10 20:28:55 +0800

    flipped left and right side of base

--- include/globals.hpp
+++ include/globals.hpp
@@ -2,14 +2,14 @@
 #define _GLOBALS_HPP_
 
 //base, closest to intake is row four, furthest is row one
-#define onel_port 4
-#define twol_port 6
-#define threel_port 7
-#define fourl_port 8
-#define oner_port 15
-#define twor_port 16
-#define threer_port 17
-#define fourr_port 18
+#define onel_port 15
+#define twol_port 16
+#define threel_port 17
+#define fourl_port 18
+#define oner_port 4
+#define twor_port 6
+#define threer_port 7
+#define fourr_port 8
 
 //catapult
 #define catal_port 9
