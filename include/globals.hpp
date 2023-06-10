48647ac3f4a8d410c7998969941855ed1cc6d368
Jj Kwan <tringpore@gmail.com>
2023-06-10 17:13:51 +0800

    base driver control

--- include/globals.hpp
+++ include/globals.hpp
@@ -1,4 +1,22 @@
 #ifndef _GLOBALS_HPP_
 #define _GLOBALS_HPP_
 
+//base, closest to intake is row four, furthest is row one
+#define onel_port 1
+#define twol_port 2
+#define threel_port 3
+#define fourl_port 4
+#define oner_port 5
+#define twor_port 6
+#define threer_port 7
+#define fourr_port 8
+
+//catapult
+#define catal_port 9
+#define catar_port 10
+
+//intake
+#define rollers_port 11
+#define flipper_port 12
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,15 @@ void on_center_button() {
 }
 
 void initialize() {
-	
+	Motor onel(onel_port, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
+	Motor twol(twol_port, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
+	Motor threel(threel_port, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
+	Motor fourl(fourl_port, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
+	Motor oner(oner_port, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
+	Motor twor(twor_port, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
+    Motor threer(threer_port, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
+    Motor fourr(fourr_port, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
 }
-
 
 void disabled() {
 
@@ -31,5 +37,42 @@ void autonomous() {
 
 
 void opcontrol() {
+    Controller master(pros::E_CONTROLLER_MASTER);
+    Motor onel(onel_port);
+	Motor twol(twol_port);
+	Motor threel(threel_port);
+	Motor fourl(fourl_port);
+	Motor oner(oner_port);
+	Motor twor(twor_port);
+    Motor threer(threer_port);
+    Motor fourr(fourr_port);
 
+    bool tankdrive = true;
+
+    while(true){
+        double left, right;
+        if(master.get_digital_new_press(DIGITAL_Y)) tankdrive = !tankdrive;
+        if(tankdrive) {
+            left = master.get_analog(ANALOG_LEFT_Y);
+            right = master.get_analog(ANALOG_RIGHT_Y);
+        } 
+                
+        else {
+        double power =  master.get_analog(ANALOG_LEFT_Y);
+        double turn = master.get_analog(ANALOG_RIGHT_X);
+        left = power + turn;
+        right = power - turn;
+        }
+
+        onel.move(left);
+        twol.move(left);
+        threel.move(left);
+        fourl.move(left);
+        oner.move(right);
+        twor.move(right);
+        threer.move(right);
+        fourr.move(right);
+
+        delay(5);
+        }
 }
