e0ca0c04bffc967159510b606e92461e41f060c9
Jj Kwan <tringpore@gmail.com>
2023-06-10 20:15:46 +0800

    working driver control

--- include/globals.hpp
+++ include/globals.hpp
@@ -2,21 +2,21 @@
 #define _GLOBALS_HPP_
 
 //base, closest to intake is row four, furthest is row one
-#define onel_port 1
-#define twol_port 2
-#define threel_port 3
-#define fourl_port 4
-#define oner_port 5
-#define twor_port 6
-#define threer_port 7
-#define fourr_port 8
+#define onel_port 4
+#define twol_port 6
+#define threel_port 7
+#define fourl_port 8
+#define oner_port 15
+#define twor_port 16
+#define threer_port 17
+#define fourr_port 18
 
 //catapult
 #define catal_port 9
-#define catar_port 10
+#define catar_port 20
 
 //intake
-#define rollers_port 11
+#define rollers_port 2
 #define flipper_port 12
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,23 @@ void on_center_button() {
 }
 
 void initialize() {
-	Motor onel(onel_port, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
-	Motor twol(twol_port, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
-	Motor threel(threel_port, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
-	Motor fourl(fourl_port, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
-	Motor oner(oner_port, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
-	Motor twor(twor_port, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
-    Motor threer(threer_port, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
-    Motor fourr(fourr_port, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
+	//base
+    Motor onel(onel_port, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
+	Motor twol(twol_port, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
+	Motor threel(threel_port, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
+	Motor fourl(fourl_port, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
+	Motor oner(oner_port, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
+	Motor twor(twor_port, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
+    Motor threer(threer_port, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
+    Motor fourr(fourr_port, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
+    
+    //catapult
+    Motor catal(catal_port, E_MOTOR_GEARSET_18, false, E_MOTOR_ENCODER_DEGREES);
+    Motor catar(catar_port, E_MOTOR_GEARSET_18, true, E_MOTOR_ENCODER_DEGREES);
+
+    //intake
+    Motor rollers(rollers_port, E_MOTOR_GEARSET_06, true, E_MOTOR_ENCODER_DEGREES);
+    Motor flipper(flipper_port, E_MOTOR_GEARSET_36, true, E_MOTOR_ENCODER_DEGREES);
 }
 
 void disabled() {
@@ -38,6 +47,8 @@ void autonomous() {
 
 void opcontrol() {
     Controller master(pros::E_CONTROLLER_MASTER);
+    
+    //base
     Motor onel(onel_port);
 	Motor twol(twol_port);
 	Motor threel(threel_port);
@@ -46,6 +57,14 @@ void opcontrol() {
 	Motor twor(twor_port);
     Motor threer(threer_port);
     Motor fourr(fourr_port);
+
+    //catapult
+    Motor catal(catal_port);
+    Motor catar(catar_port);
+
+    //intake
+    Motor rollers(rollers_port);
+    Motor flipper(flipper_port);
 
     bool tankdrive = true;
 
@@ -73,6 +92,35 @@ void opcontrol() {
         threer.move(right);
         fourr.move(right);
 
-        delay(5);
+        catal.move(120 * (master.get_digital(DIGITAL_R1)));
+		catar.move(120 * (master.get_digital(DIGITAL_R1)));
+
+		rollers.move(120 * (master.get_digital(DIGITAL_L1) - master.get_digital(DIGITAL_L2)));
+
+        flipper.move(100 * (master.get_digital(DIGITAL_X) - master.get_digital(DIGITAL_B)));
+
+        //double flipper_up = 0;
+        //double flipper_down = -100;
+        //bool flipper_lifted = true;
+
+        /*
+        if(master.get_digital(DIGITAL_R2)){
+            if(flipper_lifted){
+                flipper.move(-50);
+                delay(5);
+                flipper_lifted = !flipper_lifted;
+            }
+
+            else{
+                flipper.move(50);
+                delay(5);
+                flipper_lifted = flipper_lifted;
+            }
         }
+        */
+        
+
+
+        delay(5);
+    }    
 }
